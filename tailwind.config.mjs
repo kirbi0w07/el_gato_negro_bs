086b4be5d49c6df7a8c2ff740a49e6e395065878
jbarbosac <jbarbosac@sre.gob.mx>
2025-05-17 19:29:14 -0600

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,10 @@
+// tailwind.config.mjs
+import { defineConfig } from 'tailwindcss'
+
+export default defineConfig({
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+})
